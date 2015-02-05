a56e77a75678920152236687dea86f3cbc051f1b
Rubén Durán Balda <rduranbal@gmail.com>
2015-02-05 22:27:15 +0100

    Rust syntax highlighting

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ My personal setup
 =================
 
 Files and tools to configure my laptop. (Work in progress)
+
+Git
+---
+
+TODO: Bash prompt showing the current branch in the repo
+
+Vim
+---
+
--- vim/apply.sh
+++ vim/apply.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "This configuration requires Vundle: https://github.com/gmarik/Vundle.vim"
+
 if [ -f ~/.vimrc ]; then
     cp ~/.vimrc ~/.vimrc.back
 fi
 
 cp vimrc ~/.vimrc
+
+echo "You should now run 'vim +PluginInstall +qall' to install the plugin(s)."
